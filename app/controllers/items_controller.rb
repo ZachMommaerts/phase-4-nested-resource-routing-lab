7f3004a852c5cc2e398d91003ab4eec4651644e9
Zach <zmommaerts@gmail.com>
2022-01-12 10:53:04 -0600

    commit tree 0a8a98313206ce35ff910f328029d239577d72f5

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,28 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      items = User.find(params[:user_id]).items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = User.find(params[:user_id]).items.find(params[:id])
+    render json: item
+  end
+
+  def create
+    item = Item.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
